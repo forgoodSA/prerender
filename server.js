1d26de0eead254cf6520a47af5de0025d9f1bb93
David Purkiss <david@forgood.co.za>
2017-07-12 09:47:06 +0200

    updates

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    pageDoneCheckTimeout: 300,
+    resourceDownloadTimeout: 120000,
+    waitAfterLastRequest: 5000,
+    jsTimeout: 30000,
 });
 
 
@@ -11,10 +15,11 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
+// server.use(require('prerender-redis-cache'));
 
 server.start();
